cd5b9e337781d4507b2625cb03f98a39a580c9dc
Kuldeep Sharma <kuldeepsharma1312@gmail.com>
2018-01-08 15:07:46 +0800

    Add files via upload

---
+++ finetune.py
@@ -0,0 +1,314 @@
+import torch
+from torch.autograd import Variable
+from torchvision import models
+import cv2
+import sys
+import numpy as np
+import torchvision
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import dataset
+from prune import *
+import argparse
+import torch.nn.init as init
+from operator import itemgetter
+from heapq import nsmallest
+import time
+
+
+from flops import *
+
+class ModifiedResNetModel(torch.nn.Module): # This class create an object 
+	def __init__(self):
+		super(ModifiedResNetModel, self).__init__()
+
+		model = models.squeezenet1_1(pretrained=True)
+		print(model)
+		self.features = model.features
+		for param in self.features.parameters():
+			param.requires_grad = False
+
+		final_conv = nn.Conv2d(512,2,kernel_size=1)
+		self.classifier = nn.Sequential( 
+			nn.Dropout(p=0.5),
+			final_conv,
+			nn.ReLU(inplace=True),
+			nn.AvgPool2d(13,stride=1))
+
+
+	def forward(self, x):
+		x = self.features(x)
+		x = self.classifier(x)
+		return x.view(x.size(0),-1)
+
+
+class FilterPrunner:
+	def __init__(self, model):
+		self.model = model
+		self.reset()
+	
+	def reset(self): # This just reset the filter_rank dictionary of the object
+		# self.activations = []
+		# self.gradients = []
+		# self.grad_index = 0
+		# self.activation_to_layer = {}
+		self.filter_ranks = {}
+
+	def forward(self, x): # ????
+		self.activations = []
+		self.gradients = []
+		self.grad_index = 0
+		self.activation_to_layer = {}
+
+		activation_index = 0 # What is activation index??
+		for layer, (name, module) in enumerate(self.model.features._modules.items()):
+			# print "x is: ",x," layer is: ",layer," name is: ",name, " module is: ", module
+			if isinstance(module, torch.nn.modules.conv.Conv2d):
+				x = module(x)
+				x.register_hook(self.compute_rank)
+				self.activations.append(x)
+				self.activation_to_layer[activation_index] = layer
+				activation_index += 1
+			elif isinstance(module, torchvision.models.squeezenet.Fire):
+				x=module(x)
+				x.register_hook(self.compute_rank)
+				self.activations.append(x)
+				self.activation_to_layer[activation_index] = layer
+				activation_index += 1
+			else:
+				x= module(x)		
+		x = self.model.classifier(x)
+		return (x.view(x.size(0),-1))
+
+	def compute_rank(self, grad):
+		activation_index = len(self.activations) - self.grad_index - 1
+		activation = self.activations[activation_index]
+		values = \
+			torch.sum((activation * grad), dim = 0, keepdim=True).\
+				sum(dim=2, keepdim=True).sum(dim=3, keepdim=True)[0, :, 0, 0].data
+		
+		# Normalize the rank by the filter dimensions
+		values = \
+			values / (activation.size(0) * activation.size(2) * activation.size(3))
+
+		if activation_index not in self.filter_ranks:
+			self.filter_ranks[activation_index] = \
+				torch.FloatTensor(activation.size(1)).zero_().cuda()
+
+		self.filter_ranks[activation_index] += values
+		self.grad_index += 1
+
+	def lowest_ranking_filters(self, num):
+		data = []
+		for i in sorted(self.filter_ranks.keys()):
+			for j in range(self.filter_ranks[i].size(0)):
+				data.append((self.activation_to_layer[i], j, self.filter_ranks[i][j]))
+
+		return nsmallest(num, data, itemgetter(2))
+
+	def normalize_ranks_per_layer(self):
+		for i in self.filter_ranks:
+			v = torch.abs(self.filter_ranks[i])
+			v = v / np.sqrt(torch.sum(v * v))
+			self.filter_ranks[i] = v.cpu()
+
+	def get_prunning_plan(self, num_filters_to_prune):
+		filters_to_prune = self.lowest_ranking_filters(num_filters_to_prune)
+		
+		# After each of the k filters are prunned,
+		# the filter index of the next filters change since the model is smaller.
+		filters_to_prune_per_layer = {}
+		for (l, f, _) in filters_to_prune:
+			if l not in filters_to_prune_per_layer:
+				filters_to_prune_per_layer[l] = []
+			filters_to_prune_per_layer[l].append(f)
+
+		for l in filters_to_prune_per_layer:
+			filters_to_prune_per_layer[l] = sorted(filters_to_prune_per_layer[l])
+			for i in range(len(filters_to_prune_per_layer[l])):
+				filters_to_prune_per_layer[l][i] = filters_to_prune_per_layer[l][i] - i
+
+		filters_to_prune = []
+		for l in filters_to_prune_per_layer:
+			for i in filters_to_prune_per_layer[l]:
+				filters_to_prune.append((l, i))
+		# print("512 flters to prune: ", filters_to_prune)
+		return filters_to_prune				
+
+class PrunningFineTuner_VGG16:
+	def __init__(self, train_path, test_path, model):
+		self.train_data_loader = dataset.loader(train_path)
+		self.test_data_loader = dataset.test_loader(test_path)
+
+		#self.test_single_loader = dataset.test_loader(test_single_path) # I am adding this line...
+
+		self.model = model
+		self.criterion = torch.nn.CrossEntropyLoss()
+		self.prunner = FilterPrunner(self.model) 
+		self.model.train()
+
+
+	def test(self):
+		self.model.eval()
+		correct = 0
+		total = 0
+
+		for i, (batch, label) in enumerate(self.test_data_loader):
+			batch = batch.cuda()
+			output = model(Variable(batch))
+			pred = output.data.max(1)[1]
+			correct += pred.cpu().eq(label).sum()
+			total += label.size(0)
+		
+		print( "Accuracy :", float(correct) / total)
+		
+		self.model.train()
+
+	def train(self, optimizer = None, epoches = 10):
+		if optimizer is None:
+			optimizer = \
+				optim.SGD(model.classifier.parameters(), 
+					lr=0.001, momentum=0.90)
+
+		for i in range(epoches):
+			print ("Epoch: ", i)
+			self.train_epoch(optimizer)
+			self.test()
+		print ("Finished fine tuning.")
+		
+
+	def train_batch(self, optimizer, batch, label, rank_filters):
+		self.model.zero_grad()
+		input = Variable(batch)
+
+		if rank_filters:
+			output = self.prunner.forward(input)
+			self.criterion(output, Variable(label)).backward() # This is I think the training part.... not sure :(
+		else:
+			self.criterion(self.model(input), Variable(label)).backward()
+			optimizer.step()
+
+	def train_epoch(self, optimizer = None, rank_filters = False):
+		for batch, label in self.train_data_loader:
+			self.train_batch(optimizer, batch.cuda(), label.cuda(), rank_filters)
+
+	def get_candidates_to_prune(self, num_filters_to_prune):
+		self.prunner.reset()
+		# print("Hello Havana: ", len(self.prunner.filter_ranks))
+		self.train_epoch(rank_filters = True)
+		# print("Hello Havana2: ",len(self.prunner.filter_ranks))
+		self.prunner.normalize_ranks_per_layer()
+		# print("Hello havana3: ",len(self.prunner.filter_ranks))
+		return self.prunner.get_prunning_plan(num_filters_to_prune)
+		
+	def total_num_filters(self):
+		filters = 0
+
+		for name, module in self.model.features._modules.items():
+			if isinstance(module, torch.nn.modules.conv.Conv2d):
+				filters = filters + module.out_channels
+			if isinstance(module, torchvision.models.squeezenet.Fire):
+				filters += module.squeeze.out_channels + module.expand1x1.out_channels + module.expand3x3.out_channels 
+
+		return filters
+
+	def prune(self):
+		#Get the accuracy before prunning
+		self.test()
+
+		self.model.train()
+
+		#Make sure all the layers are trainable
+		for param in self.model.features.parameters():
+			param.requires_grad = True
+
+		number_of_filters = self.total_num_filters()
+		print("Total number of filters: ",number_of_filters)
+		num_filters_to_prune_per_iteration = 128
+		iterations = int(float(number_of_filters) / num_filters_to_prune_per_iteration)
+
+		iterations = int(iterations * 2.0 / 4)
+		print ("Number of prunning iterations to reduce 67% filters", iterations)
+		for _ in range(iterations):
+			print ("Ranking filters.. ")
+			prune_targets = self.get_candidates_to_prune(num_filters_to_prune_per_iteration) # Get filters to prune...
+			layers_prunned = {}
+			for layer_index, filter_index in prune_targets:
+				if layer_index not in layers_prunned:
+					layers_prunned[layer_index] = 0
+				layers_prunned[layer_index] = layers_prunned[layer_index] + 1 
+
+			print ("Layers that will be prunned", layers_prunned)
+			print ("Prunning filters.. ")
+			model = self.model.cpu()
+			for layer_index, filter_index in prune_targets:
+				# print "layer_index: ",layer_index, " ada ", filter_index
+				model = prune_vgg16_conv_layer(model, layer_index, filter_index)
+
+			self.model = model.cuda()
+
+			message = str(100*float(self.total_num_filters()) / number_of_filters) + "%"
+			print ("Filters prunned", str(message))
+			# self.test()\
+			_, conv__ = model.features._modules.items()[3]
+			print(conv__.squeeze.weight.data)
+			print ("Fine tuning to recover from prunning iteration.")
+			optimizer = optim.SGD(self.model.parameters(), lr=0.001, momentum=0.9) # ......
+			self.train(optimizer, epoches = 5)
+			# print("final shape:  ", model.parameters())
+
+		print ("Finished. Going to fine tune the model a bit more")
+		self.train(optimizer, epoches = 15)
+		torch.save(model, "model_prunned")
+
+
+def test_single(model,test_single_path):
+	test_single_loader = dataset.test_loader(test_single_path)
+	model.eval()
+	time_ = 0 
+	for i, (batch, label) in enumerate(test_single_loader):
+		batch = batch.cuda()
+		t0 = time.time()
+		output = model(Variable(batch))
+		t1 = time.time()			
+		time_ += t1-t0
+		pred = output.data.max(1)[1]
+	print ("Avg time taken per image is(over 800 Images..)  ", time_/800)
+def get_args():
+	parser = argparse.ArgumentParser()
+	parser.add_argument("--model", type = str, default = "model_prunned")
+	parser.add_argument("--train", dest="train", action="store_true")
+	parser.add_argument("--prune", dest="prune", action="store_true")
+	parser.add_argument("--run", dest="run",action="store_true")
+	parser.add_argument("--flops", dest="flops",action="store_true")
+	parser.add_argument("--train_path", type = str, default = "train")
+	parser.add_argument("--test_path", type = str, default = "test")
+	parser.add_argument("--test_single_path", type = str, default = "test_single")
+	parser.set_defaults(train=False)
+	parser.set_defaults(prune=False)
+	args = parser.parse_args()
+	return args
+
+if __name__ == '__main__':
+	args = get_args()
+
+	if args.flops:
+		main_(torch.load("model_prunned").cuda())
+	elif args.run:
+		model = torch.load(args.model).cuda()
+		test_single(model, args.test_path)
+	else:	
+		if args.train:
+			model = ModifiedResNetModel().cuda() # Create a model
+		elif args.prune:
+			model = torch.load("model").cuda()
+
+		fine_tuner = PrunningFineTuner_VGG16(args.train_path, args.test_path, model)
+
+		if args.train:
+			fine_tuner.train(epoches = 20) # 
+			torch.save(model, "model")
+
+		elif args.prune:
+			fine_tuner.prune()
---
+++ flops.py
@@ -0,0 +1,95 @@
+import torchvision
+import re
+import torch
+
+in_height = 224
+in_width = 224
+
+def get_num_gen(gen):
+    return sum(1 for x in gen)
+
+def flops_layer(layer):
+    """
+    Calculate the number of flops for given a string information of layer.
+    We extract only resonable numbers and use them.
+    
+    Args:
+        layer (str) : example
+            Linear (512 -> 1000)
+            Conv2d(3, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+            BatchNorm2d(64, eps=1e-05, momentum=0.1, affine=True)
+    """
+    #print(layer)
+    idx_type_end = layer.find('(')
+    type_name = layer[:idx_type_end]
+    
+    params = re.findall('[^a-z](\d+)', layer)    
+    flops = 1
+    
+    if layer.find('Linear') >= 0:
+        C1 = int(params[0])
+        C2 = int(params[1])
+        flops = C1*C2
+        
+    elif layer.find('Conv2d') >= 0:
+        C1 = int(params[0])
+        C2 = int(params[1])
+        K1 = int(params[2])
+        K2 = int(params[3])
+        # print(len(params))
+        global in_height
+        global in_width
+        flops = C1*C2*K1*K2*in_height*in_width
+        
+        if len(params)<=6: 
+            in_height = (in_height-K1 + 2*0)/int(params[4]) +1
+            in_width = (in_width - K2 + 2*0)/int(params[4]) +1
+        else:
+            in_height = (in_height-K1 + 2*int(params[6]))/int(params[4]) +1
+            in_width = (in_width - K2 + 2*int(params[6]))/int(params[4]) +1
+
+    elif layer.find('Pool') >=0:
+        C1 = int(params[0])
+        C2 = int(params[2])
+        if C2==0:
+            C2=1
+        global in_height
+        global in_width
+        in_height = (in_height - C1)/C2 + 1
+        in_width = (in_width - C1)/C2 + 1
+
+    return flops
+
+def calculate_flops(gen):
+    """
+    Calculate the flops given a generator of pytorch model.
+    It only compute the flops of forward pass.
+    
+    Example:
+        >>> net = torchvision.models.resnet18()
+        >>> calculate_flops(net.children())
+    """
+    flops = 0;
+    
+    for child in gen:
+        num_children = get_num_gen(child.children())
+        
+        # leaf node
+        if num_children == 0:
+            flops += flops_layer(str(child))
+        
+        else:
+            flops += calculate_flops(child.children())
+    
+    return flops
+
+#net = torchvision.models.resnet18()
+def main_(model=None, input_height=224, input_width=224):
+
+
+    in_height=input_height
+    in_width=input_width    
+    net = model
+    flops = calculate_flops(net.children())
+    print(flops / 10.0**9, 'Billion')
+# 11.435429919 G
